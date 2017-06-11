1981731017dc2f0e6fb13c1bb04290b98fb547a7
Michael Duve <mduve@designmail.net>
2017-06-11 12:48:50 +0200

    build-system works now

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
@@ -6,6 +6,7 @@ var clean = require('gulp-clean');
 var spawn = require('child_process').spawn;
 var gls = require('gulp-live-server');
 var mergeStream = require('merge-stream');
+var env = require('gulp-env');
 
 function runSequence( tasks ) {
     if( !tasks || tasks.length <= 0 ) return;
@@ -53,7 +54,7 @@ gulp.task('paella-opencast:compile.release', ['paella-opencast:prepare'], functi
 });
 
 
-gulp.task('paella-opencast:build', ["paella-opencast:compile.debug"], function () {
+gulp.task('paella-opencast:build', ['set-prod-env', "paella-opencast:compile.debug"], function () {
     runSequence(['paellaCustomBuild', 'beuth']);
 });
 
@@ -72,19 +73,51 @@ gulp.task('default', ['paella-opencast:build']);
 
 /* CUSTOM */
 gulp.task('beuth', function () {
-    var s1 = gulp.src('beuth/build/paella-opencast/scss/**/*.scss')
+    if (process.env.NODE_ENV === 'production') {
+        gulp.src('build/paella-opencast/info', {read: false}).pipe(clean());
+        gulp.src('build/paella-opencast/search', {read: false}).pipe(clean());
+    }
+        var s1 = gulp.src('beuth/build/paella-opencast/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions', '> 1%', 'IE > 8', 'Firefox ESR'],
             cascade: false
         }))
         .pipe(gulp.dest('beuth/build/paella-opencast/css/'));
-    var s2 = gulp.src('beuth/build/**/*').pipe(gulp.dest('build/'));
+
+    var filesToCopy = ['beuth/build/**/*', '!beuth/build/paella-opencast/{scss,scss/**}'];
+
+    if (process.env.NODE_ENV !== 'development') {
+        filesToCopy.push('!beuth/build/paella-opencast/{info,info/**}');
+        filesToCopy.push('!beuth/build/paella-opencast/{search,search/**}');
+    }
+
+    var s2 = gulp.src(filesToCopy).pipe(gulp.dest('build/'));
 
     return mergeStream(s1, s2);
 });
 
-gulp.task('beuth-watch', function () {
-    gulp.run('paella-opencast:server');
-    gulp.watch('beuth/build/paella-opencast/**/*', ['paella-opencast:beuth']);
+gulp.task('beuth-serve', ['set-dev-env'], function() {
+    var server = gls.static('build/paella-opencast', 8000);
+    server.start();
+    gulp.watch(['beuth/build/paella-opencast/**/*'], function(file) {
+        gulp.start('beuth');
+        server.notify.apply(server, [file]);
+    });
+});
+
+gulp.task('set-dev-env', function() {
+    env({
+        vars: {
+            NODE_ENV: "development"
+        }
+    });
+});
+
+gulp.task('set-prod-env', function() {
+    env({
+        vars: {
+            NODE_ENV: "production"
+        }
+    });
 });
